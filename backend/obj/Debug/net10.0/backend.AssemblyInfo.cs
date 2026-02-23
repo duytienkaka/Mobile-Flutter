9917acb02e62bb3b472e499578ac459361a1b9e2
DuongVanViet <vanviet2102005@gmail.com>
2026-02-23 13:15:14 +0700

    Clean gitignore

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39d25bbecc385bac60aebd29f00df4a81d1bd62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5119a10ce6f4d057d6f2dba6255ab5ccd47e5303")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
